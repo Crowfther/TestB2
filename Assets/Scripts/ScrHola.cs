4cd81374ddeb085f3bb760e3b9845aad91fc82d3
Aitor Gonzalez <gonzalezca@campusemav.com>
2020-10-05 11:02:52 +0200

    Clase  5/10/2020
    Controlador Player

---
+++ Assets/Scripts/ScrHola.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrHola : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        print("Hola!");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Saluda();
+    }
+
+    void Saluda()
+    {
+        print("Hola");
+    }
+}
---
+++ Assets/Scripts/ScrPlayer.cs
@@ -0,0 +1,29 @@
+﻿using JetBrains.Annotations;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrPlayer : MonoBehaviour
+{
+    [SerializeField]
+    float velocidad=3;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x, y; //Para leer el gamepad.
+
+        x = Input.GetAxis("Horizontal");    //Leer movimiento horizontal.
+        y = Input.GetAxis("Vertical");      //Leer movimiento vertical.
+
+        //print ("Horizontal:" + x + " / Vertical: " + y);
+
+        transform.Translate(x*Time.deltaTime*velocidad, y*Time.deltaTime*velocidad, 0);
+    }
+}
