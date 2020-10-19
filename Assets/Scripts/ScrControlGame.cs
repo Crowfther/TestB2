2f4d33255b83e81227aadd52862d0208192f128c
Aitor Gonzalez <gonzalezca@campusemav.com>
2020-10-19 09:58:55 +0200

    Clase 19/10/2020
    Hasta puntuacion de pickup

---
+++ Assets/Scripts/ScrControlGame.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrControlGame : MonoBehaviour
+{
+    public static int puntos = 0; // Puntuación.
+}
--- Assets/Scripts/ScrPlayer.cs
+++ Assets/Scripts/ScrPlayer.cs
@@ -6,24 +6,38 @@ using UnityEngine;
 public class ScrPlayer : MonoBehaviour
 {
     [SerializeField]
-    float velocidad=3;
 
-    // Start is called before the first frame update
+    float fuerza = 3;
+    float x, y; //Para leer el gamepad.
+    
+    Rigidbody2D rb; // Para acceder al componente RigidBody
+
+
     void Start()
     {
-    
+        rb = GetComponent<Rigidbody2D>(); // rb apunta al componente Rigidbody2D del jugador.
     }
 
     // Update is called once per frame
     void Update()
     {
-        float x, y; //Para leer el gamepad.
-
         x = Input.GetAxis("Horizontal");    //Leer movimiento horizontal.
         y = Input.GetAxis("Vertical");      //Leer movimiento vertical.
+    }
 
-        //print ("Horizontal:" + x + " / Vertical: " + y);
+    private void FixedUpdate()
+    {
+        rb.AddForce(new Vector2(x*fuerza, y*fuerza));
+    }
 
-        transform.Translate(x*Time.deltaTime*velocidad, y*Time.deltaTime*velocidad, 0);
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        // if (collision.tag == "Pickup") Destroy(collision.gameObject);
+        if (collision.CompareTag("Pickup"))
+        {
+            Destroy(collision.gameObject); // Optimización de la orden anterior.
+            ScrControlGame.puntos += 5;
+            print ("Puntuación: " + ScrControlGame.puntos);
+        }
     }
 }
